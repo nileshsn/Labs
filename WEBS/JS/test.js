07a369b368b821961c514986fb04ea033ae6ca58
ENUGANDHULA NILESH <nileshenugandhula@gmail.com>
2025-01-26 22:00:03 +0530

    Add files via upload

---
+++ WEBS/JS/test.js
@@ -0,0 +1,3 @@
+var tweet = prompt("Compose a messsage");
+var tweetCount = tweet.length;
+console.log("You have written "+tweetCount+" characters, you have "+(140-tweetCount)+" characters remaining");
